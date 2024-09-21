df1a73a79bc1307fdc9dc319879211571175cde0
jatnerubia <jasonlerit077@gmail.com>
2024-09-21 10:40:55 +0800

    update mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -2,4 +2,8 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+-- nvim-tree
+map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
+
+-- exit
 map("i", "jk", "<ESC>")
