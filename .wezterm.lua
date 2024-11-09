cf0170ca0f3348ec3efe72d7ab4840b5e10750d6
jatnerubia <jasonlerit077@gmail.com>
2024-11-09 19:55:24 +0800

    update dotfiles

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,29 +1,11 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
 config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 12
 
 config.color_scheme = "Catppuccin Mocha"
-
--- config.colors = {
--- 	background = "#181818",
--- }
-
--- config.background = {
--- 	{
--- 		source = {
--- 			File = "/Users/j/Pictures/bg1.jpg",
--- 		},
--- 		hsb = {
--- 			brightness = 0.1,
--- 		},
--- 	},
--- }
 
 config.cursor_thickness = 2
 config.underline_thickness = 5
@@ -32,10 +14,6 @@ config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
 
--- config.window_background_opacity = 0.85
--- config.macos_window_background_blur = 100
-
 config.force_reverse_video_cursor = true
 
--- and finally, return the configuration to wezterm
 return config
--- .zshrc
+++ .zshrc
@@ -116,4 +116,3 @@ export ANDROID_HOME=$HOME/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 export XDG_CONFIG_HOME="$HOME/.config"
-
